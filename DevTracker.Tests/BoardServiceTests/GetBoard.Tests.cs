906a3466b8624a7703572acd507bdbbb03244f63
Alixandroaie Constantin <constantin.alixandroaie@gmail.com>
2025-09-23 10:24:19 +0300

    Added some more tests

--- DevTracker.Tests/BoardServiceTests/GetBoard.Tests.cs
+++ DevTracker.Tests/BoardServiceTests/GetBoard.Tests.cs
@@ -1,5 +1,39 @@
-﻿namespace DevTracker.Application.Tests.BoardServiceTests;
+﻿using DevTracker.Contracts;
+using DevTracker.Core;
+using DevTracker.Data.Models;
+using NSubstitute;
 
-public class GetBoardTests
+namespace DevTracker.Application.Tests.BoardServiceTests;
+
+public class GetBoardTests : TestBase
 {
+    [Fact]
+    public async Task GetBoards_RepoReturnsFailure_ExpectFailure()
+    {
+        //Arrange
+        const string errorMessage = "Get Boards failed.";
+        var repoResult = Result<Board>.Failure(ErrorType.Unexpected, errorMessage);
+        _boardRepo.GetBoardAsync(Arg.Any<long>())
+            .Returns(Task.FromResult(repoResult));
+        //Act
+        var response = await _sut.GetBoardAsync("1");
+        //Assert
+        Assert.Equal(Result.Failure, response.Result);
+        Assert.Equal(errorMessage, response.ErrorMessage);
+    }
+
+    [Fact]
+    public async Task GetBoards_RepoReturnsSuccess_ExpectSuccess()
+    {
+        //Arrange
+        var repoResult = Result<Board>.Success([]);
+        _boardRepo.GetBoardAsync(Arg.Any<long>())
+            .Returns(Task.FromResult(repoResult));
+        //Act
+        var response = await _sut.GetBoardAsync("1");
+
+        //Assert
+        Assert.Equal(Result.Success, response.Result);
+        Assert.Null(response.ErrorMessage);
+    }
 }
