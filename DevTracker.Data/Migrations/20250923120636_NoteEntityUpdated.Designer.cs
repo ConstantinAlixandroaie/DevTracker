6a7885fd22aaaa2d5a939de138ad4374a425e7b0
Alixandroaie Constantin <constantin.alixandroaie@gmail.com>
2025-09-23 15:08:38 +0300

    Updated DB

--- DevTracker.Data/Migrations/20250923120636_NoteEntityUpdated.Designer.cs
+++ DevTracker.Data/Migrations/20250923120636_NoteEntityUpdated.Designer.cs
@@ -4,6 +4,7 @@ using DevTracker.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DevTracker.Data.Migrations
 {
     [DbContext(typeof(DevTrackerContext))]
-    partial class DevTrackerContextModelSnapshot : ModelSnapshot
+    [Migration("20250923120636_NoteEntityUpdated")]
+    partial class NoteEntityUpdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
